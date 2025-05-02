e07e6f3682ca32b51e61b9a153dcec461847be55
NoahN08 <noahnbusinessinquiries@gmail.com>
2025-05-02 14:28:59 -0600

    Update level.py

--- levels/level.py
+++ levels/level.py
@@ -1,1 +1,21 @@
+import pygame
+from settings import *
 
+class Level:
+    def __init__(self, game):
+        self.game = game
+        self.background = None
+        self.player = None
+        self.enemies = []
+        
+    def update(self, dt):
+        pass
+        
+    def draw(self, screen):
+        pass
+        
+    def handle_collisions(self):
+        pass
+        
+    def reset(self):
+        pass
