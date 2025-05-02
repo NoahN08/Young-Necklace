8098a2ba16f5865b0558df2328e9a11eb377906f
NoahN08 <noahnbusinessinquiries@gmail.com>
2025-05-02 14:27:28 -0600

    Update inventory.py

--- items/inventory.py
+++ items/inventory.py
@@ -1,1 +1,23 @@
+class Inventory:
+    def __init__(self):
+        self.weapons = []
+        self.armor = []
+        self.consumables = []
+        
+    def add_item(self, item):
+        if item.type in ["sword", "bow", "quiver"]:
+            self.weapons.append(item)
+        elif item.type in ["helmet", "chest", "gloves", "boots"]:
+            self.armor.append(item)
+        else:
+            self.consumables.append(item)
+            
+    def has_weapon(self, weapon_name):
+        return any(w.name == weapon_name for w in self.weapons)
+        
+    def get_weapon(self, weapon_name):
+        for weapon in self.weapons:
+            if weapon.name == weapon_name:
+                return weapon
+        return None
 
